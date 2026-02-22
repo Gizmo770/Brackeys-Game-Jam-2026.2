b5d4c8f9fa9a2b307b120737caa5c25a70c4cd2f
bhayden51 <bradley8451@hotmail.com>
2026-02-21 18:05:16 -0600

    Obstacles

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public float speedLoss;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public List<Sprite> sprites;
+
+    private Vector2 moveDirection;
+    private int rotateDirection;
+
+    void Start()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+        rotateDirection = Random.Range(1,2) == 0 ? 1 : -1;
+        moveDirection = Random.insideUnitCircle.normalized;
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + rotateDirection * rotateSpeed * Time.deltaTime);
+        transform.Translate(moveSpeed * moveDirection * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            PlayerMovement playerMovement = collision.GetComponent<PlayerMovement>();
+
+            playerMovement.ApplySpeedLossFromObstacle(speedLoss);
+            Destroy(gameObject);
+        }
+    }
+}
